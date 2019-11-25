4a5642622544ec96fc9c83f238b0e8ed206aa94e
Samantha Smith <smith.samantha.lynn@gmail.com>
2019-11-25 22:45:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
 function theBeatlesPlay(musicians, instruments) {
   var array = [];
   for (var i = 0; i < musicians.length; i++) {
-    array.push(musicians[i] + " plays " + instruments[i]);
+  	array.push(musicians[i] + ' plays ' + instruments[i]);
   }
   return array;
 }
 
-function johnLennonFacts() {
-  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
-  var array = [];
-  var i = 0;
-  while (array.length < facts.length) {
-    array.push(facts[i] + "!!!");
-    i++;
+function johnLennonFacts(facts) {
+  var results = [];
+  var count = 0;
+  while (count < facts.length) {
+    results.push(facts[count] + "!!!");
+    count++;
   }
-  return array;
+  return results;
 }
 
-function iLoveTheBeatles(number) {
+function iLoveTheBeatles(num) {
   var array = [];
-  }
   do {
-    array.push("I love the Beatles!");
-    number += 1
-  } while (number < 15);
+    array.push("I love the Beatles!")
+    num++;
+  } while (num < 15);
   return array;
+}
